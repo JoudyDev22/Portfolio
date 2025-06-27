a5fef2f60a85c303b5c52664d06363456932c284
joudy <dawalibijoudy@gmail.com>
2025-06-28 00:05:06 +0300

    update

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -12,7 +12,7 @@ export default function Cart(props){
         <Card.Body>
         <Card.Title style={{textAlign:"start",fontWeight:"bold"}}>{props.title}</Card.Title>
         <Card.Text style={{textAlign:"start",fontSize:"18px"}}>{props.desc}.</Card.Text>
-        <Card.Text style={{textAlign:"start",fontWeight:"bold"}}><span style={{color:"rgb(13 110 253)"}}>Built With :</span> {props.lang}</Card.Text>
+        <Card.Text style={{textAlign:"start",fontWeight:"bold"}}><span style={{color:"rgb(13 110 253)"}}>Built using :</span> {props.lang}</Card.Text>
         <div style={{display:"flex" ,gap:"3px"}}>
         <Button variant="dark"  ><Link to={props.link} style={{textDecoration:"none",color:"white"}}>< Github ></Github>  Github</Link></Button>
         <Button variant="primary"  ><a href={props.pre} style={{textDecoration:"none",color:"white",marginLeft:"4px"}}> <Eye></Eye> Live Preview</a></Button>
